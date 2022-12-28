d2ad73621055c1827901bdfce05e7053bf8b1522
Eliza Thomas <elizamthomas1994@gmail.com>
2022-12-28 21:19:23 +0000

    Modifiedbirthday_app file and added birthday_check.erb file

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ source "https://rubygems.org"
 
 # gem "rails"
 
-gem "sinatra", "~> 3.0"
-gem "sinatra-contrib", "~> 3.0"
 gem "webrick", "~> 1.7"
 gem "rack-test", "~> 2.0"
+
+gem "sinatra", "~> 3.0"
+
+gem "sinatra-contrib", "~> 3.0"
--- birthday_app.rb
+++ birthday_app.rb
@@ -1,23 +1,41 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'date'
 
 class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
   end
 
-  # Declares a route that responds to a request with:
-  #  - a GET method
-  #  - the path /
   get '/' do
     return erb(:index)
   end
 
   post '/submit' do
-    name = params[:name]
-    birth_date = params[:birth_date]
-    birth_month = params[:birth_month]
+    session[:name] = params[:name]
+    session[:birth_date] = params[:birth_date]
+    session[:birth_month] = params[:birth_month]
+    redirect '/birthday_check'
+  end
+
+  get '/birthday_check' do
+    @name = session[:name]
+    @birth_date = session[:birth_date]
+    @birth_month = session[:birth_month]
+
+    @day = @birth_date.to_i
+    @month = @birth_month.to_i
+
+    current_date = DateTime.now
+    given_date = DateTime.new(2022, @month, @day)
+
+    if current_date > given_date
+      given_date = DateTime.new(2023, @month, @day)
+    end
+
+    @days_to_go = (current_date..given_date).count
+    erb(:birthday_check)
   end
 end
